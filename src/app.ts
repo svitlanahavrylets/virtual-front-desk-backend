dc9861964b70dcee967b182cb2510a905a58313e
Svitlana Havrylets <svitlana.havrylets@gmail.com>
2026-02-09 22:01:18 +0100

    add vercel link

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,10 @@ import cors from "cors";
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://virtual-front-desk-frontend-seven.vercel.app",
+    ],
     credentials: true,
   }),
 );
